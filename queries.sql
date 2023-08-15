fdd4c7d157f59497b75d0ff12ca17ada610b2d83
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-08-15 09:07:36 +0500

    Record updated and rollback in a transaction statement

--- queries.sql
+++ queries.sql
@@ -8,3 +8,25 @@ SELECT name, escape_attempts from animals where weight_kg > 10.5;
 SELECT * from animals where neutered = true;
 SELECT * from animals where name NOT IN ('Gabumon');
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals
+SET species = 'Unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'Digimon' WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'Pokemon' WHERE species IS NULL OR species = '';
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
